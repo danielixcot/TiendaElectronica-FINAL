43215ff62987a9fdac53da192d03dc963260fec8
Daniel Ixcot <daniel2005311@gmail.com>
2024-06-06 08:12:44 -0600

    Add project files.

---
+++ TiendaElectronica/Dispositivo.cs
@@ -0,0 +1,14 @@
+﻿namespace TiendaElectronica
+{
+    public class Dispositivo
+    {
+        public int Serie { get; set; }
+        public string Marca { get; set; }
+        public int Modelo { get; set; }
+        public Dispositivo() { 
+            Serie = 0;
+            Marca = string.Empty;
+            Modelo = 0;
+        }
+    }
+}
---
+++ TiendaElectronica/Laptop.cs
@@ -0,0 +1,12 @@
+﻿namespace TiendaElectronica
+{
+    public class Laptop : Dispositivo
+    {
+        public int Ram {  get; set; }
+        public int almacenamiento { get; set; }
+        public Laptop() { 
+            Ram = 0;
+            almacenamiento = 0;
+        }
+    }
+}
